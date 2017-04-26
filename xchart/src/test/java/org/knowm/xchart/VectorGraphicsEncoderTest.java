cc54eaa11dd9533eacc6b028bc898b7d5953d3d8
Ekkart Kleinod <ekkart.kleinod@fokus.fraunhofer.de>
2017-04-26 13:28:03 +0200

    test of file name extension method of VectorGraphicsEncoderTest

--- xchart/src/test/java/org/knowm/xchart/VectorGraphicsEncoderTest.java
+++ xchart/src/test/java/org/knowm/xchart/VectorGraphicsEncoderTest.java
@@ -18,6 +18,7 @@ package org.knowm.xchart;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.knowm.xchart.VectorGraphicsEncoder.VectorGraphicsFormat;
 
 /**
  * @author ekleinod
@@ -27,8 +28,26 @@ public class VectorGraphicsEncoderTest {
 	@Test
 	public void testAddFileExtension() {
 
-		Assert.assertEquals("test", "test");
+		for (VectorGraphicsFormat format : VectorGraphicsFormat.values()) {
 
+			// test -> test.svg
+			Assert.assertEquals(String.format("test.%s", format.toString().toLowerCase()), VectorGraphicsEncoder.addFileExtension("test", format));
+
+			// test.svg -> test.svg
+			Assert.assertEquals(String.format("test.%s", format.toString().toLowerCase()), VectorGraphicsEncoder.addFileExtension(String.format("test.%s", format.toString().toLowerCase()), format));
+
+			// test.svgsvg -> test.svgsvg.svg
+			Assert.assertEquals(String.format("test.%1$s%1$s.%1$s", format.toString().toLowerCase()), VectorGraphicsEncoder.addFileExtension(String.format("test.%1$s%1$s", format.toString().toLowerCase()), format));
+
+			// test.svg.svg -> test.svg.svg
+			Assert.assertEquals(String.format("test.%1$s.%1$s", format.toString().toLowerCase()), VectorGraphicsEncoder.addFileExtension(String.format("test.%1$s.%1$s", format.toString().toLowerCase()), format));
+
+			// test.txt -> test.txt.svg
+			Assert.assertEquals(String.format("test.txt.%1$s", format.toString().toLowerCase()), VectorGraphicsEncoder.addFileExtension("test.txt", format));
+
+		}
+
+		VectorGraphicsEncoder.addFileExtension("test.txt", null);
 
 	}
 
